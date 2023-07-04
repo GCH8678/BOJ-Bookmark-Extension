18a320d9e5df3a2b70e36b1a48baf6edfc0a3cb7
GCH8678 <gch8678@gmail.com>
2023-07-04 22:47:47 +0900

    chore : Spring Rest Docs 도입을 위한 빌드 업무 수정
    Spring Rest Docs 도입을 위해 관련된 Gradle.build 파일 수정 및 gitignore 내용 추가

--- bojbm_backend/src/test/java/com/ch/bojbm/BojbmApplicationTests.java
+++ bojbm_backend/src/test/java/com/ch/bojbm/BojbmApplicationTests.java
@@ -2,11 +2,13 @@ package com.ch.bojbm;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "/application.yml")
 class BojbmApplicationTests {
 
-	@Test
+	//@Test
 	void contextLoads() {
 	}
 
